8463635fe05adbd045f61ffdd124fd4865c6310e
BugeezS <brunolb.lafont@gmail.com>
2023-04-28 11:47:37 +0200

    add random

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,16 +1,27 @@
 const list = document.querySelector("ul");
 const items = list.children;
 
+let fastAndFuriousItem;
 for (let i = 0; i < items.length; i++) {
   const item = items[i];
-
-  if (item.nodeType === 1) {
-    if (item.textContent.includes("Fast and Furious")) {
-      if (i !== 0) {
-        list.insertBefore(item, items[0]);
-      }
-      item.classList.add("important");
+  if (item.nodeType === 1 && item.textContent.includes("Fast and Furious")) {
+    fastAndFuriousItem = item;
+    if (i !== 0) {
+      list.insertBefore(item, items[0]);
     }
+    item.classList.add("important");
+  }
+  for (let j = i + 1; j < items.length; j++) {
+    const nextItem = items[j];
+    if (nextItem.nodeType === 1 && item.textContent === nextItem.textContent) {
+      list.removeChild(nextItem);
+    }
+  }
+}
+
+for (let i = 0; i < items.length; i++) {
+  const item = items[i];
+  if (item.nodeType === 1) {
     item.addEventListener("click", function () {
       if (item.textContent.includes("Fast and Furious")) {
         alert(
@@ -21,11 +32,24 @@ for (let i = 0; i < items.length; i++) {
       }
     });
   }
+}
 
-  for (let j = i + 1; j < items.length; j++) {
-    const nextItem = items[j];
-    if (nextItem.nodeType === 1 && item.isEqualNode(nextItem)) {
-      list.removeChild(nextItem);
+document.body.addEventListener("keyup", function (event) {
+  if (event.key === "r") {
+    const shuffledItems = shuffle(
+      [...items].filter((item) => item !== fastAndFuriousItem)
+    );
+    for (let i = 0; i < shuffledItems.length; i++) {
+      const item = shuffledItems[i];
+      list.insertBefore(item, fastAndFuriousItem.nextSibling);
     }
   }
+});
+
+function shuffle(array) {
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+  return array;
 }
